9841dc48f3deb6b74fd2f65724859d1bf309937e
Mirze26 <mirzaab@code.edu.az>
2023-04-12 00:31:34 +0400

    Elearn

---
+++ Elearn-temp/Elearn/Areas/Admin/Controllers/CourseController.cs
@@ -0,0 +1,31 @@
+﻿using Elearn_temp.Data;
+using Elearn_temp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Elearn_temp.Areas.Admin.Controllers
+{
+
+    [Area("Admin")]
+
+    public class CourseController : Controller
+    {
+
+
+
+        private readonly AppDbContext _context;
+
+        public CourseController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+
+        public async Task<IActionResult> Index()
+        {
+            IEnumerable<Course> courses = await _context.Courses.Include(m => m.CourseImages).Include(m => m.Author).Where(m => !m.SoftDelete).ToListAsync();
+
+            return View(courses);
+        }
+    }
+}
